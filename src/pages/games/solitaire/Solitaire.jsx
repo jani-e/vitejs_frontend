d334766d41801ff87088c507c09539b70d1c2b1d
jani-e <j.eriksson91@gmail.com>
2023-06-17 22:17:30 +0300

    Moved card svg into Card component and is draggable

--- src/pages/games/solitaire/Solitaire.jsx
+++ src/pages/games/solitaire/Solitaire.jsx
@@ -1,31 +1,53 @@
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import Card from 'src/components/Card'
+
 const Solitaire = () => {
+  const suits = {
+    hearts: '♥',
+    diamonds: '♦',
+    clubs: '♣',
+    spades: '♠'
+  }
+
+  const tempSuits = {
+    hearts: '♡',
+    diamonds: '♢',
+    clubs: '♧',
+    spades: '♤'
+  }
+
+  const style = {
+    backgroundColor: 'green'
+  }
 
   return (
-    <div>
-      <h2>Solitaire</h2>
-      <svg width={100} height={150}>
-        <rect width={100} height={150} rx={15} fill="orange"/>
-        <text x={80} y={20} fill="white">9</text>
-        <text x={5} y={140} fill="white">9</text>
-        <text x={5} y={20} fill="white">♥</text>
-        <text x={80} y={140} fill="white">♥</text>
-      </svg>
+    <div style={style}>
+      <DndProvider backend={HTML5Backend}>
+        <h2>Solitaire</h2>
+        <Card value={2} suit={suits.hearts} color='red' tempSuit={tempSuits.hearts} />
+        <Card value={3} suit={suits.diamonds} color='red' tempSuit={tempSuits.diamonds} />
+        <Card value={4} suit={suits.clubs} color='black' tempSuit={tempSuits.clubs} />
+        <Card value={5} suit={suits.spades} color='black' tempSuit={tempSuits.spades} />
+      </DndProvider>
     </div>
   )
 }
 
 /*
 todo / things to consider
- - can svg be dragged and moved like in solitaire game?
-   - card needs to be draggable
-   - card should move with the user's cursor when dragged
-   - cards should have fixed places
- - game code logic
-   - tracking where cards are
-   - double click card logic
-   - hidden card / reveal card
-   - deck
- - move card svg/image to assets or components?
+- can svg be dragged and moved like in solitaire game?
+  - Card that is a div and contains svg can be dragged --> react DnD
+  - cards should have fixed places
+- card styling
+  - hidden card / reveal card -> useState?
+  - card colors and 'tempSuit' added in suits
+  - value display logic for 1 ace, 11 jack, 12 queen, 13 king
+- game code logic
+  - tracking where cards are
+  - double click card logic
+  - deck
+- move card svg/image to assets or components? --> moved to components 
 */
 
 export default Solitaire
