e6d782421b2b34f9c56436bd858ea48b998fcad3
jani-e <j.eriksson91@gmail.com>
2023-07-08 18:05:46 +0300

    Created navigation menu bar with MUI AppBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import { Link, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import 'src/App.css'
-import NavLinks from 'src/components/NavLinks'
+import NavBar from 'src/components/NavBar'
 import Home from 'src/pages/Home'
 import Games from 'src/pages/Games'
 import Solitaire from 'src/pages/games/Solitaire'
 import TBA from 'src/pages/games/TBA'
 
-
-
 const App = () => {
   return (
     <>
-      <Link to='/'>
-        <h1>Jani-e&apos;s website</h1>
-      </Link>
-      <NavLinks></NavLinks>
+      <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
         <Route path='/games' element={<Games />} />
         <Route path='/games/solitaire' element={<Solitaire />} />
         <Route path='/games/tba' element={<TBA />} />
