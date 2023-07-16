4e3db1c2fa0a4812fc219b8e9f718a6706e5da93
jani-e <j.eriksson91@gmail.com>
2023-07-16 21:29:55 +0300

    TreeView styling

--- src/components/TreeView.jsx
+++ src/components/TreeView.jsx
@@ -1,15 +1,24 @@
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 const TreeView = ({pages, parent = null}) => {
   const filteredPages = pages.filter(page => page.parent === parent)
   if (filteredPages.length === 0) {
     return null
   }
+
+  const listStyle = {
+    listStyleType: 'none',
+    paddingInlineStart: '15px' //change to 0px after lines logic
+  }
+
+  console.log(filteredPages.length - 1) // if last: &#x2517; ┗, otherwise &#x2523; ┣
+
   return (
-    <ul>
+    <ul style={listStyle}>
       {filteredPages.map(page => (
         <li key={page.id}>
-          {page.name}
+          {page.parent !== null && <span>&#x2523; &#x2501; </span>}<Link to={page.name}>{page.name}</Link>
           {<TreeView pages={pages} parent={page.id} />}
         </li>
       ))}
@@ -23,3 +32,14 @@ TreeView.propTypes = {
   pages: PropTypes.array.isRequired,
   parent: PropTypes.number
 }
+
+/*
+&#x2523; ┣
+&#x2517; ┗
+&#x2501; ━
+&#x2503; ┃
+
+TODO:
+improve logic for the lines
+linking is based on page.name -> doesn't work with submenu items, missing parent from path
+*/
